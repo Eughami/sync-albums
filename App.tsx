b38cd1b077f36c9607a721a4c6f5a6451747944e
Ares <ares.r@mblhightech.com>
2023-12-28 14:18:31 +0200

    Added upload button

--- App.tsx
+++ App.tsx
@@ -1,66 +1,164 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {PermissionsAndroid, Platform} from 'react-native';
+import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import axios from 'axios';
+import {BASE_URL} from '@env';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+async function hasAndroidPermission() {
+  const getCheckPermissionPromise = () => {
+    if (typeof Platform.Version === 'number' && Platform.Version >= 33) {
+      return Promise.all([
+        PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+        ),
+        PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ),
+      ]).then(
+        ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+          hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+      );
+    } else {
+      return PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      );
+    }
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  const hasPermission = await getCheckPermissionPromise();
+  if (hasPermission) {
+    return true;
+  }
+  const getRequestPermissionPromise = () => {
+    if (typeof Platform.Version === 'number' && Platform.Version >= 33) {
+      return PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+        PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+      ]).then(
+        statuses =>
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+            PermissionsAndroid.RESULTS.GRANTED,
+      );
+    } else {
+      return PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      ).then(status => status === PermissionsAndroid.RESULTS.GRANTED);
+    }
+  };
+
+  return await getRequestPermissionPromise();
 }
 
 function App(): React.JSX.Element {
+  const [filename, setFilename] = React.useState('');
+  const [uploading, setUploading] = React.useState(false);
+  const [error, setError] = React.useState(null);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  async function listAlbums() {
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return;
+    }
+    let err = null;
+    setUploading(true);
+    const albums = await CameraRoll.getAlbums({
+      assetType: 'All',
+      albumType: 'All',
+    });
+
+    let albumsNames = albums.map(album => album.title);
+    let lastTime = await AsyncStorage.getItem('timestamp');
+    console.log(lastTime);
+    if (!lastTime) {
+      lastTime = '1703196000000'; // December 22, 2023 00:00:00  GMT+02:00
+    }
+    albumsNames = albumsNames.filter(name =>
+      [
+        'Twitter',
+        'Pictures',
+        'PS App',
+        // 'Screenshots',
+        // 'WhatsApp Video',
+        // 'WhatsApp Images',
+        'Exercices',
+        'Messenger',
+        // 'WhatsApp Animated Gifs',
+        'Snapchat',
+        'Instander',
+        // 'twitter',
+        'Reddit',
+        // 'WhatsApp Documents',
+        'Camera',
+        'Download',
+        'God of War',
+        'Massages',
+        'Food',
+      ].includes(name),
+    );
+
+    for (const name of albumsNames) {
+      const content = await CameraRoll.getPhotos({
+        first: 100,
+        groupTypes: 'Album',
+        groupName: name,
+        // fromTime: 1703548800000,
+        // fromTime: Date.now() - 5 * 60 * 60 * 1000,
+        fromTime: parseInt(lastTime, 10),
+      });
+      if (content.edges.length > 0) {
+        for (const image of content.edges) {
+          const formdata = new FormData();
+          setFilename(image.node.image.uri.split('/').pop() ?? '');
+          formdata.append('files', {
+            name: `${name}&&${
+              image.node.timestamp * 1000
+            }&&${image.node.image.uri.split('/').pop()}`,
+            type: image.node.type,
+            uri: image.node.image.uri,
+          });
+          await axios({
+            method: 'post',
+            url: 'https://sync.eughami.com/upload',
+            // url: 'http://20.0.30.191:8000/upload',
+            data: formdata,
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+            .then(res => console.log(res.data))
+            .catch(e => {
+              err = e.message;
+              console.log(e?.response?.data);
+            });
+        }
+      }
+    }
+    setError(err);
+    if (!err) {
+      await AsyncStorage.setItem('timestamp', Date.now().toString());
+    }
+    setUploading(false);
+    setFilename('');
+  }
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -68,50 +166,29 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+
+      <View
+        style={{
+          ...styles.sectionContainer,
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <Text>URL: {BASE_URL}</Text>
+        {uploading && <Text>{filename}</Text>}
+        {error && <Text style={styles.error}>{JSON.stringify(error)}</Text>}
+        <Button title="Sync" onPress={listAlbums} />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    height: '100%',
+    justifyContent: 'center',
+    padding: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  error: {
+    color: 'red',
   },
 });
 
